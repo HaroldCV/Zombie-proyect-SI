80d7dab34df1574f11b73ae21446979f64fe310d
HaroldCV <70811385+HaroldCV@users.noreply.github.com>
2023-06-02 18:32:08 -0500

    estilos aplicados_falta_corregir dashboard.js

--- frontend_/src/components/dashboard.js
+++ frontend_/src/components/dashboard.js
@@ -12,21 +12,34 @@ function Dashboard() {
   };
 
   return (
-    <div>
-      <h1>Welcome to the Dashboard</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About Us</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact Us</Link>
-          </li>
-          <li>{username}</li>
-        </ul>
+    <div className="container">
+      <h1 className="mt-4">Welcome to the Dashboard</h1>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <Link to="/dashboard" className="navbar-brand">Home</Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link to="/about" className="nav-link">About Us</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/contact" className="nav-link">Contact Us</Link>
+              </li>
+            </ul>
+          </div>
+          <div>{username}</div>
+        </div>
       </nav>
       {/* Renderiza el componente PaymentForm y pasa la función handlePayment como prop */}
       <PaymentForm handlePayment={handlePayment} />
