57df877f32b1be947b54e5d5ce971c22cb687657
HaroldCV <harold.canto@utec.edu.pe>
2023-06-02 11:17:21 -0500

    changes

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import StripeCheckout from 'react-stripe-checkout';
 
 function Dashboard() {
   // Aquí deberías obtener el nombre de usuario del estado o de tu sistema de autenticación
   const username = 'NombreUsuario';
 
+  const handlePayment = (token) => {
+    // Aquí puedes enviar el token de pago a tu backend para procesar la compra
+    console.log(token);
+  };
+
   return (
     <div>
-      <h1>Welcome to the Dashboard</h1>
       <nav>
         <ul>
           <li>
-            <Link to="/dashboard">Home</Link>
+            <Link to="/">Home</Link>
           </li>
           <li>
             <Link to="/about">About Us</Link>
@@ -22,6 +27,18 @@ function Dashboard() {
           <li>{username}</li>
         </ul>
       </nav>
+      <h1>Welcome to the Dashboard</h1>
+      {/* Agrega el contenido adicional de tu dashboard */}
+      <StripeCheckout
+        stripeKey="tu_stripe_public_key"
+        token={handlePayment}
+        name="Mi Tienda"
+        description="Realiza tu compra"
+        amount={1000} // Monto en centavos
+        currency="USD"
+      >
+        <button>Realizar Compra</button>
+      </StripeCheckout>
     </div>
   );
 }
