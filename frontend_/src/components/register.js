631be102df40f73661f65601bc0a772f1d05242b
HaroldCV <70811385+HaroldCV@users.noreply.github.com>
2023-06-02 17:45:46 -0500

    añadidos_estilos_register.js

--- frontend_/src/components/register.js
+++ frontend_/src/components/register.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Register() {
   const navigate = useNavigate();
@@ -8,7 +10,6 @@ function Register() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
 
   const handleRegister = async () => {
     try {
@@ -21,26 +22,79 @@ function Register() {
 
       if (response.data.success) {
         // Registro exitoso
-        console.log(response.data.message);
-        navigate('/login'); // Redireccionar al login
+        Swal.fire({
+          icon: 'success',
+          title: 'Registration Successful',
+          text: response.data.message,
+        }).then(() => {
+          navigate('/login'); // Redireccionar al login
+        });
       } else {
         // Error en el registro
-        setErrorMessage(response.data.message);
+        Swal.fire({
+          icon: 'error',
+          title: 'Registration Error',
+          text: response.data.message,
+        });
       }
     } catch (error) {
       console.log(error);
+      Swal.fire({
+        icon: 'error',
+        title: 'Registration Error',
+        text: 'An error occurred during registration.',
+      });
     }
   };
 
   return (
-    <div>
-      <h1>Register</h1>
-      {errorMessage && <p>{errorMessage}</p>}
-      <input type="text" placeholder="Full Name" value={fullname} onChange={(e) => setFullname(e.target.value)} />
-      <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
-      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <button onClick={handleRegister}>Register</button>
+    <div className="container d-flex justify-content-center align-items-center vh-100">
+      <div className="card p-4 w-50">
+        <h1 className="mb-4">Register</h1>
+        <div className="mb-3">
+          <label htmlFor="fullname" className="form-label">Full Name</label>
+          <input
+            type="text"
+            className="form-control"
+            id="fullname"
+            value={fullname}
+            onChange={(e) => setFullname(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="username" className="form-label">Username</label>
+          <input
+            type="text"
+            className="form-control"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">Email</label>
+          <input
+            type="text"
+            className="form-control"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <button className="btn btn-primary" onClick={handleRegister}>
+          Register
+        </button>
+      </div>
     </div>
   );
 }
